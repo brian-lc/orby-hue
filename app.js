605f90c529fdde21a65650ec6ebb8797478d6343
Brian Chamberlain <blchamberlain@gmail.com>
2014-10-23 22:54:41 -0700

    Able to send rgb values to the orb, very basic form

--- app.js
+++ app.js
@@ -2,10 +2,39 @@ var express = require('express');
 
 var app = express();
 var http = require('http');
+app.set('views', './views')
+app.set('view engine', 'jade')
 
-app.route('/color')
-.get(function(req, res, next){
-  res.send('red');
+// Middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser());
+
+// DB
+var Firebase = require('firebase');
+
+// Request library
+var request = require('request');
+
+// keys
+var fbURL = process.env.FB_URL;
+var coreId = process.env.CORE_ID
+var accessToken = process.env.ACCESS_TOKEN
+var sparkCoreURL = "https://api.spark.io/v1/devices/"
+
+app.route('/')
+.get(function (req, res, next){
+  console.log('Rendered color form');
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
 })
+.post(function (req, res, next){
+  var r = req.param('rVal');
+  var g = req.param('gVal');
+  var b = req.param('bVal');
+  var rgbStr = r + g + b;
+  console.log('Sending color ', rgbStr);
+  request.post(sparkCoreURL + coreId + "/color").form({access_token:accessToken, params: rgbStr});
+  res.render('index', { title: 'Hey', message: 'Color '+ r + ':' + g + ':' + b + ' sent!'});
+
+});
 
 http.createServer(app).listen(process.env.PORT || 3000);
